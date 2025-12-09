d44d0063fadf12b414ed4ce1c119a49e928d17c0
bhanu <bhanu.praneeth@paroscale.com>
2025-12-09 06:19:12 +0000

    refactor(CP): Add support to store NISD ID's as csv in ChunkNisd struct
    	- niova-client expects the nisd id's to be in comma separated format
    	  so it can parse the xml data easily.
    	- so replaced nisd id's slice with a single comma separated list

--- controlplane/ctlplanefuncs/server/srvctlplanefuncs.go
+++ controlplane/ctlplanefuncs/server/srvctlplanefuncs.go
@@ -1887,9 +1887,12 @@ func ReadChunkNisd(args ...interface{}) (interface{}, error) {
 		NumReplicas: uint8(len(rqResult.ResultMap)),
 	}
 	for _, v := range rqResult.ResultMap {
-		chunkInfo.NisdUUID = append(chunkInfo.NisdUUID, string(v))
+		ids = append(ids, string(v))
 	}
-	chunkInfo.NumReplicas = uint8(len(chunkInfo.NisdUUID)) - 1
+	chunkInfo := ctlplfl.ChunkNisd{
+		NisdUUIDs:   strings.Join(ids, ","),
+		NumReplicas: uint8(len(rqResult.ResultMap)) - 1,
+	}
 
 	log.Debugf("ReadChunkNisd: returning chunk-nisd info for vdev %s chunk %s", vdevID, chunk)
 	return ctlplfl.EncodeResponse(chunkInfo)
