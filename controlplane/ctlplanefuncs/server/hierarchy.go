1dd7459fde97e359c2598b9e38763db48fa432fa
bhanu <bhanu.praneeth@paroscale.com>
2026-05-25 05:28:25 +0000

    chore(CP): remove unused compareNisd method

--- controlplane/ctlplanefuncs/server/hierarchy.go
+++ controlplane/ctlplanefuncs/server/hierarchy.go
@@ -46,7 +46,6 @@ type Hierarchy struct {
 var HR Hierarchy
 
 func compareEntity(a, b *Entities) bool          { return a.ID < b.ID }
-func compareNisd(a, b *cpLib.Nisd) bool          { return a.ID < b.ID }
 func compareDevice(a, b *cpLib.DeviceAlloc) bool { return a.ID < b.ID }
 
 // Initialize the Hierarchy Struct
